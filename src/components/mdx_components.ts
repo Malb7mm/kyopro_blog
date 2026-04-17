8fa1e00766c3b20fa7a53357371d5091bc89e5ea
Malb7mm <ykmeak9g@gmail.com>
2026-04-17 21:03:47 +0900

    update

--- src/components/mdx_components.ts
+++ src/components/mdx_components.ts
@@ -5,4 +5,6 @@ import Faint from "./markup/Faint.astro";
 import Red from "./markup/Red.astro";
 import Block from "./markup/Block.astro";
 
-export const components = { Animator, CollectionLink, Faint, Red, Block };
+import { CldImage } from 'astro-cloudinary';
+
+export const components = { Animator, CollectionLink, Faint, Red, Block, CldImage };
